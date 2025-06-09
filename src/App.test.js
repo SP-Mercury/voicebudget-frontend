0709d994848398fb8a253551473d399ea91bfb92
SP-Mercury <mizunomercury0910@gmail.com>
2025-06-09 18:25:04 +0800

    重新提交，補上漏掉的 components 等檔案

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import VoiceBudgetApp from './components/VoiceUploader';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+function App() {
+  return <VoiceBudgetApp />;
+}
+
+export default App;
